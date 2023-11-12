c0c0b4e23cfddd7b5cfe4508112009887b563344
danGitRys <daniel@familie-ryssel.de>
2023-11-12 15:49:13 +0100

    PROJ-41 <Verbleibendes Models erstellt>

--- project_management_tool/models.py
+++ project_management_tool/models.py
@@ -35,4 +35,61 @@ class Employee(models.Model):
         db_table = 'employee'
 
 
+class Booking(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    fk_employee = models.BigIntegerField()
+    fK_positon = models.BigIntegerField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    pause = models.FloatField()
+    time = models.IntegerField()
+
+    class Meta:
+        db_table = 'booking'
+
+class Forecast(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    fk_employee = models.BigIntegerField()
+    fk_position = models.BigIntegerField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    info = models.TextField()
+
+    class Meta:
+        db_table = 'forecast'
+
+class Positon(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    position_id = models.TextField(max_length=50)
+    fk_project = models.BigIntegerField()
+    rate = models.FloatField()
+    wd = models.FloatField()
+    volume_total = models.FloatField()
+    volume_remaining = models.FloatField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'position'
+
+class Project(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    p_id = models.TextField(max_length=50)
+    name = models.TextField(max_length=50)
+    company = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    fk_creator = models.BigIntegerField()
+    creation_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'project'
     
+
+class Team(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.TextField(max_length=50)
+    info = models.TextField()
+
+    class Meta:
+        db_table = 'team' 
